0e1fdd991d8513d9c43c5a7a194052dd41d327da
Sultan Mukhtarov <suli419956@gmail.com>
2025-07-08 00:10:47 +0600

    Initial Commit.

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import AppBar from "./UI/AppBar/AppBar.vue";
+</script>
+
+<template>
+  <header>
+    <AppBar />
+  </header>
+  <main>
+    Main input
+  </main>
+</template>
+
---
+++ src/UI/AppBar/AppBar.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import '../AppBar/appbarStyle/style.css'
+import {ref} from "vue";
+
+const search = ref('');
+
+const clearInput = () => {
+  search.value = '';
+};
+</script>
+
+<template>
+  <header class="app-bar">
+    <RouterLink to="/" @click.prevent="clearInput" class="app-title">ToDo</RouterLink>
+    <DayNightButton />
+  </header>
+  <div class="date-time">
+    {{
+      new Date().toLocaleDateString('en-us', {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+      })}}
+  </div>
+</template>
